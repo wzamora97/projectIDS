b0aec634de3764e5da9f0ed90fb99933cbf26c2d
Unknown <wzamora@mail.csuchico.edu>
2017-12-08 23:00:14 -0800

    Added sentiment analysis
    Added positive and negative words and tried running sentiment analysis

--- twitterProj.r
+++ twitterProj.r
@@ -32,6 +32,55 @@ tweets <- do.call(rbind,anything)
 tweets <- filter(tweets, geo_enabled == TRUE)
 tweets$lat <- ifelse(is.na(tweets$lat), tweets$place_lat, tweets$lat)
 tweets$lon <- ifelse(is.na(tweets$lon), tweets$place_lon, tweets$lon)
+
+good_text <- scan('C:/Users/willi/Desktop/DSI/twitter/projectIDS/positive.txt',
+            what='character', comment.char=';')
+
+bad_text <- scan('C:/Users/willi/Desktop/DSI/twitter/projectIDS/negative.txt',
+            what='character', comment.char=';')
+
+score.sentiment <- function(sentences, good_text, bad_text, .progress='none')
+{
+  require(plyr)
+  require(stringr)
+  # we got a vector of sentences. plyr will handle a list
+  # or a vector as an "l" for us
+  # we want a simple array of scores back, so we use
+  # "l" + "a" + "ply" = "laply":
+  scores = laply(sentences, function(sentence, good_text, bad_text)   {
+    
+    # clean up sentences with R's regex-driven global substitute, gsub():
+    sentence = gsub('[[:punct:]]', '', sentence)
+    sentence = gsub('[[:cntrl:]]', '', sentence)
+    sentence = gsub('\\d+', '', sentence)
+    #to remove emojis
+    sentence <- iconv(sentence, 'UTF-8', 'ASCII')
+    sentence = tolower(sentence)
+    
+    # split into words. str_split is in the stringr package
+    word.list = str_split(sentence, '\\s+')
+    # sometimes a list() is one level of hierarchy too much
+    words = unlist(word.list)
+    
+    # compare our words to the dictionaries of positive & negative terms
+    pos.matches = match(words, good_text)
+    neg.matches = match(words, bad_text)
+    
+    # match() returns the position of the matched term or NA
+    # we just want a TRUE/FALSE:
+    pos.matches = !is.na(pos.matches)
+    neg.matches = !is.na(neg.matches)
+    
+    # and conveniently enough, TRUE/FALSE will be treated as 1/0 by sum():
+    score = sum(pos.matches) - sum(neg.matches)
+    
+    return(score)
+  }, good_text, bad_text, .progress=.progress )
+  
+  scores.df = data.frame(score=scores, text=sentences)
+  return(scores.df)
+}
+sentimentxmastweets <- score.sentiment(anything, good_text, bad_text, .progress='text')
 #states <- map.where("state", tweets$lon, tweets$lat)
 #head(sort(table(states), decreasing=TRUE))
 ## First create a data frame with the map data 
@@ -56,4 +105,3 @@ ggplot(map.data) + geom_map(aes(map_id = region), map = map.data, fill = "grey90
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         plot.background = element_blank()) 
-
